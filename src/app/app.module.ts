021ed9bcfe9f238a6ed7fb2c7dcd8cf0067e6329
masha <marmylad@gmail.com>
2021-12-14 13:13:05 +0200

    добавлена русская локализация

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TicketCalculationComponent } from './ticket-calculation/ticket-calculation.component';
 import {FormsModule} from "@angular/forms";
 
+import {registerLocaleData} from "@angular/common";
+import localeRu from '@angular/common/locales/ru';
+import localeRuExtra from '@angular/common/locales/extra/ru';
+registerLocaleData(localeRu, 'ru-RU', localeRuExtra);
 
 @NgModule({
   declarations: [
@@ -15,7 +19,10 @@ import {FormsModule} from "@angular/forms";
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'ru-Ru'},
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'RUB'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
