9a612e480f8da60a3102367026068be505040bab
masha <marmylad@gmail.com>
2021-12-14 11:54:49 +0200

    добавлен компонент ticket-calculation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TicketCalculationComponent } from './ticket-calculation/ticket-calculation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketCalculationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ticket-calculation/ticket-calculation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-calculation',
+  templateUrl: './ticket-calculation.component.html',
+  styleUrls: ['./ticket-calculation.component.scss']
+})
+export class TicketCalculationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
